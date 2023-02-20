6fa359497295531088322110009c66b3959c6bcb
limseungchae <tmdco12@naver.com>
2023-02-20 12:13:13 +0900

    게시판 - 새글쓰기

--- static/js/join.js
+++ static/js/join.js
@@ -2,10 +2,10 @@
 const processJoin = () => {
     let frm = document.join;
     if (frm.uid.value === '') alert('아이디는?');
-    if (frm.pwd.value === '') alert('비밀번호는?');
-    if (frm.repwd.value === '') alert('비밀번호 확인은?');
-    if (frm.name.value === '') alert('이름은?');
-    if (frm.email.value === '') alert('이메일?');
+    else if (frm.pwd.value === '') alert('비밀번호는?');
+    else if (frm.repwd.value === '') alert('비밀번호 확인은?');
+    else if (frm.name.value === '') alert('이름은?');
+    else if (frm.email.value === '') alert('이메일?');
     else {
         frm.method = 'post';
         frm.submit();
---
+++ views/partials/dbconfig.js
@@ -0,0 +1,1 @@
+module.exports = {user: 'bigdata', password: 'bigdata',connectString: "43.201.55.203:1521/XE"}
