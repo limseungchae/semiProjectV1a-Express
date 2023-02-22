6b9e56cd8d4f7d0292af842be3aba39129e3129e
limseungchae <tmdco12@naver.com>
2023-02-22 13:54:37 +0900

    시도, 구군, 읍면동 출력2 - 시도 : 시군구

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const path = require('path');
-
-router.get('/', (req, res) => {
-    res.render('index', {title: '첫 화면'});
-});
-
-module.exports = router;
--- views/partials/dbconfig.js
+++
@@ -1,1 +0,0 @@
-module.exports = {user: 'bigdata', password: 'bigdata',connectString: "43.201.55.203:1521/XE"}
